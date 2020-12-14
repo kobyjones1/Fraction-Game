6604130abd7516e57ac842480e3c27d1e51eb131
Koby <kobyjonesofficial@gmail.com>
2020-12-14 04:38:45 -0500

    Create GUI

--- src/fracGame.java
+++ src/fracGame.java
@@ -1,8 +1,30 @@
+//import java.util.Scanner;
 
-public class fracGame {
+import java.awt.GridLayout;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class fracGame extends JFrame{
+	
+	//static Scanner userInput = new Scanner(System.in);
+	
 	public static void main(String[] args) {
-		int numerator1, numerator2;
+		JFrame mainDisplay = new JFrame("Fraction Game");
+		mainDisplay.setSize(500, 500);
+		mainDisplay.setLocationRelativeTo(null);
+		mainDisplay.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JButton startButton = new JButton("Start");
+		
+		JPanel mainPanel = new JPanel(new GridLayout(1,1));
+		mainPanel.add(startButton);
+		
+		mainDisplay.setContentPane(mainPanel);
+		mainDisplay.setVisible(true);
+		
+		//int numerator1, numerator2;
 		
 	}
 
